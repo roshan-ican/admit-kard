a111c84e08e56d8bfe2991d19f6dbe29f54d14c8
Muhammad Roshan ali <66337567+roshan-ican@users.noreply.github.com>
2022-05-29 14:13:56 +0530

    front page done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-import DateTime from './dateTime';
+// import DateTime from './dateTime';
+// import './style.css';
+import Form from './form';
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
-      <DateTime />
+      {/* <h1>Hello World!</h1>
+      <DateTime /> */}
+      <Form />
     </div>
   );
 }
---
+++ src/form.jsx
@@ -0,0 +1,76 @@
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import Select from 'react-select';
+import makeAnimated from 'react-select/animated';
+
+const options = [
+  { value: 'Canada', label: 'Canda' },
+  { value: 'USA', label: 'USA' },
+  { value: 'Australia', label: 'Australia' },
+  { value: 'New-Zealand', label: 'New-Zeland' },
+  { value: 'UK', label: 'UK' },
+  { value: 'Germany', label: 'Germany' },
+  { value: 'Ireland', label: 'Ireland' },
+];
+
+const courseLevel = [
+  { value: 'UG', label: 'UG' },
+  { value: 'PG', label: 'PG' },
+];
+
+const animatedComponents = makeAnimated();
+
+export function Form() {
+  const { register, handleSubmit, control } = useForm();
+
+  return (
+    <div>
+      <form
+        onSubmit={handleSubmit((data) => {
+          console.log(data);
+        })}
+      >
+        <input
+          {...register('firstName', { required: true })}
+          placeholder="Full Name"
+        />
+        <input
+          type="text"
+          placeholder="Email"
+          {...register('Email', { required: true, pattern: /^\S+@\S+$/i })}
+        />
+        <input
+          type="tel"
+          placeholder="Mobile number"
+          {...register('Mobile number', {
+            required: true,
+            minLength: 6,
+            maxLength: 12,
+          })}
+        />
+
+        <Select
+          name="Course Level"
+          placeholder="Course Level"
+          closeMenuOnSelect={false}
+          components={animatedComponents}
+          defaultInputValue={[courseLevel[2], courseLevel[2]]}
+          options={courseLevel}
+        />
+
+        <Select
+          name="Country Preferences"
+          placeholder="Country Preferences"
+          closeMenuOnSelect={false}
+          components={animatedComponents}
+          options={options}
+          isMulti
+        />
+
+        <input type="submit" />
+      </form>
+    </div>
+  );
+}
+
+export default Form;
