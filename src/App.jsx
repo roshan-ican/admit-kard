b10efc0075fbe583b248d1c28c1b01d4ef4ffa69
Muhammad Roshan ali <66337567+roshan-ican@users.noreply.github.com>
2022-05-29 10:41:52 +0530

    the dateTime

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import DateTime from './dateTime';
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World!</h1>
+      <DateTime />
+    </div>
+  );
+}
+
+export default App;
